a243b9cf0c7cf6672f9c3ced56443026e62114f3
Michael <michael.walton@g.austincc.edu>
2024-01-15 14:58:52 -0600

    partial addComponent method implementation

--- ECS/ComponentManager.cpp
+++ ECS/ComponentManager.cpp
@@ -2,7 +2,18 @@
 
 void ComponentManager::registerComponent() {
     Component newComponent = availableComponents.front(); // Assign first available id to new component
+    availableComponents.pop(); // Remove selected id from available id's
     std::array<ComponentType, MAX_ENTITIES> newComponentData{}; // Make array for new component's data
-    componentData.push_back(newComponentData); // Add data to componentData array
-    componentArrayKey.insert({newComponent, componentData.size() - 1}); // Pair new id with new array in key
+    componentDataArray.push_back(newComponentData); // Add data to componentData array
+    componentArrayKey.insert({newComponent, componentDataArray.size() - 1}); // Pair new id with new array in key
+}
+
+void ComponentManager::addComponent(Entity entityID, Component componentID) {
+    auto it = componentArrayKey.find(componentID); // Check if there is an entry in key with the component id
+    
+    if (it != componentArrayKey.end()) { // if ID is in key, get index for componentDataArray
+        int dataIndex = it->second;
+    }
+
+    
 }
