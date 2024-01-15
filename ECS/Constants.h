5ff571582e6a060dee6812f134878239c205d1ab
Michael <michael.walton@g.austincc.edu>
2024-01-15 11:04:09 -0600

    defined ComponentType as std::any

--- ECS/Constants.h
+++ ECS/Constants.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <bitset>
+#include <any>
 
-using Entity = int; // Defines an entity as a 32-bit integer
+using Entity = int; // Define an entity as a 32-bit integer
 const Entity MAX_ENTITIES = 5000;
 
-using Component = int; // Defines a component as an 8-bit integer
+using Component = int; // Define a component as an 8-bit integer
 const Component MAX_COMPONENTS = 50;
 
-using ComponentTracker = std::bitset<MAX_COMPONENTS>; // Defines ComponentTracker as a bitset that will store whether or not an entity has each component
+using ComponentTracker = std::bitset<MAX_COMPONENTS>; // Define ComponentTracker as a bitset
+
+using ComponentType = std::any; // Define Component Type as any type
