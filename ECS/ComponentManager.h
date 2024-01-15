9a6f9d1d793c97e21822c6641be706267d4ce65d
Michael <michael.walton@g.austincc.edu>
2024-01-15 14:58:32 -0600

    added qtyComponents member

--- ECS/ComponentManager.h
+++ ECS/ComponentManager.h
@@ -10,15 +10,16 @@ class ComponentManager {
 
     public:
 
-    void registerComponent();
-    void addComponent(Entity);
-    void removeComponent(Entity, Component);
+        void registerComponent();
+        void addComponent(Entity, Component);
+        void removeComponent(Entity, Component);
 
     private:
 
-        std::vector<std::array<ComponentType, MAX_ENTITIES>> componentData{};
+        std::vector<std::array<ComponentType, MAX_ENTITIES>> componentDataArray{};
         std::unordered_map<int, int> componentArrayKey{};
         std::queue<Component> availableComponents{};
+        int qtyComponents = componentDataArray.size();
 };
 
         
